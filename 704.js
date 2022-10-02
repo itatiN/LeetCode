9e123647be9cc111278115553bada89795bcd230
Itatiano Niquini <itaticontato@gmail.com>
2022-10-02 02:55:28 -0300

    704

--- 704.js
+++ 704.js
@@ -1,20 +1,14 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
- var search = function(nums, target) {
+var search = function(nums, target) {
     var left = 0;
     var rigth = nums.length-1;
-    
+        
     while(left <= rigth){
-        var mid = left + (rigth-left)/2;
-        if(nums[mid] == target){
-            return mid;
-        }else if(nums[mid] > target){
-            rigth =  mid - 1;
-        }else{
-            left = mid - 1;
-        }
+        var mid = left + Math.floor((rigth-left)/2);
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target)
+            left = mid + 1;
+        else
+            rigth =  mid - 1;        
     }
-};
+    return -1;
+}
